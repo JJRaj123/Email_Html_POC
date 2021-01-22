855d424d3ec0f1df2ed93467a0969e98414730fb
Jothiraj Mothiraj <jmothiraj@worldbank.org>
2021-01-22 20:47:15 +0530

    Updated

--- Email_Html_POC/Services/EmailNotificationService.cs
+++ Email_Html_POC/Services/EmailNotificationService.cs
@@ -9,6 +9,7 @@ using Email_Html_POC.Utilities.Common;
 using System.IO;
 using SendGrid;
 using SendGrid.Helpers.Mail;
+using System.Text;
 
 namespace Email_Html_POC.Services
 {
@@ -36,7 +37,32 @@ namespace Email_Html_POC.Services
                     //var htmlContent = _commonHelperService.FormEmailBody() != null ? _commonHelperService.FormEmailBody() : "";
                     var htmlContent = _commonHelperService.FormEmailBody() != null ? _commonHelperService.FormEmailBody() : "";
                     var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                    //msg.AddBcc("jjeee20@gmail.com");
+                    //msg.AddCc("jjeee20@gmail.com");
+                    //string dataDir = Directory.GetCurrentDirectory();
+                    //msg.AddAttachment("CheckDoc", dataDir + "\\html_test.PDF");
+                    //msg.Personalizations[0].Bccs = new List<EmailAddress>();
+                    //msg.Personalizations[0].Bccs.Add(new EmailAddress("jjeee20@gmail.com"));
+                    msg.AddBcc(new EmailAddress("jmothiraj@worldbank.org", "bcc"));
+                    msg.AddBcc(new EmailAddress("kmitaigiri@worldbank.org", "cc"));
+                    ///Dynamically resolving the content and generate the pdf//
+                    byte[] byteData =_commonHelperService.ConvertToPDF();
+                    ///Can add multiple attachments but need to pass file type, file name, byte content//
+                    msg.Attachments= new List<SendGrid.Helpers.Mail.Attachment>
+                    {
+                    new SendGrid.Helpers.Mail.Attachment
+                    {
+                        Content = Convert.ToBase64String(byteData),
+                        Filename = "FILE_NAME.pdf",
+                        Type = "application/pdf",
+                        Disposition = "attachment"
+                    }
+                   };
                     var response = await client.SendEmailAsync(msg);
+                    if (response.StatusCode >= System.Net.HttpStatusCode.BadRequest)
+                    {
+                        throw new Exception(response.StatusCode.ToString());
+                    }
                     return true;
                 }
                 else
